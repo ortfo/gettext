ce1176f25885b36397ef038d61e6b31a92e57a3a
Ewen Le Bihan <hey@ewen.works>
2023-12-24 18:27:24 +0100

    sort serialized po file correctly
    using sort.SliceStable and collator comparisons. setting a source language is also required. we sort by msgid than by msgctxt

--- po/file.go
+++ po/file.go
@@ -10,14 +10,18 @@ import (
 	"io"
 	"io/ioutil"
 	"sort"
+
+	"golang.org/x/text/collate"
+	"golang.org/x/text/language"
 )
 
 // File represents an PO File.
 //
 // See http://www.gnu.org/software/gettext/manual/html_node/PO-Files.html
 type File struct {
-	MimeHeader Header
-	Messages   []Message
+	MimeHeader     Header
+	Messages       []Message
+	SourceLanguage language.Tag
 }
 
 // Load loads po file format data.
@@ -37,6 +41,7 @@ func LoadFile(path string) (*File, error) {
 func loadData(data []byte) (*File, error) {
 	r := newLineReader(string(data))
 	var file File
+	file.SetSourceLanguage(language.English)
 	for {
 		var msg Message
 		if err := msg.readPoEntry(r); err != nil {
@@ -58,13 +63,20 @@ func (f *File) Save(name string) error {
 	return ioutil.WriteFile(name, []byte(f.String()), 0666)
 }
 
+func (f *File) SetSourceLanguage(language language.Tag) {
+	f.SourceLanguage = language
+}
+
 // Save returns a po file format data.
 func (f *File) Data() []byte {
 	// sort the massge as ReferenceFile/ReferenceLine field
 	var messages []Message
 	messages = append(messages, f.Messages...)
-	sort.Slice(messages, func(i, j int) bool {
-		return messages[i].less(&messages[j])
+	collator := collate.New(f.SourceLanguage, collate.Loose)
+	sort.SliceStable(messages, func(i, j int) bool {
+		msgid := collator.CompareString(messages[i].MsgId, messages[j].MsgId)
+		msgctx := collator.CompareString(messages[i].MsgContext, messages[j].MsgContext)
+		return msgid < 0 || (msgid == 0 && msgctx < 0)
 	})
 
 	var buf bytes.Buffer
